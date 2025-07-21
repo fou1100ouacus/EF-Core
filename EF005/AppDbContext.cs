71adf2522ff9930800f4b8aafe4a54cfaff72873
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-07-21 15:33:44 +0300

    Add files via upload

---
+++ EF005/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbContext;
+namespace EFCore01
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Wallet> Wallets { get; set; } = null! ;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var Configuation = new ConfigurationBuilder()
+                .AddJsonFile("appsettingss.json").Build();
+
+
+            var conecstr = Configuation.GetSection("conecstr").Value;
+            optionsBuilder.UseSqlServer(conecstr);
+
+
+        }
+    }
+}
---
+++ EF005/Class1.cs
@@ -0,0 +1,115 @@
+﻿
+
+using EFCore01;
+using System;
+using System.Data;
+using System.Transactions;
+
+namespace EF005
+{
+
+    class Program
+    {
+        public static void Main()
+        {
+
+
+
+            //__________________________  EF CORE             ____________________________
+            //__________________________  EF CORE  01 Rertive data           ____________________________
+
+            //using (var context = new AppDbContext())
+            //{
+            //    foreach (var item in context.Wallets)
+            //    {
+            //        Console.WriteLine(item);
+
+            //    }
+
+            //}
+
+            //__________________________  EF CORE  05 Rertive data           ____________________________
+
+            //using (var context = new AppDbContext())
+            //{
+            //    var item=context.Wallets.FirstOrDefault(x=>x.Id==3);
+            //    Console.WriteLine(item);
+
+            //}
+
+            //__________________________  EF CORE  05 Insert data           ____________________________
+
+
+            //var wallet = new Wallet
+            //{
+            //    Holder = "manar",
+            //    Balance = 9000
+            //};
+            //using (var context=new AppDbContext())
+            //{
+            //    context.Add(wallet);
+            //    context.SaveChanges();
+            //}
+            //__________________________  EF CORE  05 Update data           ____________________________
+
+            //using (var context = new AppDbContext())
+            //{
+            // var wallet=context.Wallets.Single(x=>x.Id==3);
+            //    wallet.Balance += 7;
+            //    context.SaveChanges();
+
+            //}
+            //__________________________  EF CORE  05 delet data           ____________________________
+
+            //using (var context = new AppDbContext())
+            //{
+            //    var wallet = context.Wallets.Single(x => x.Id == 5);
+            //    context.Wallets.Remove(wallet);
+            //    context.SaveChanges();
+
+            //}
+            //__________________________  EF CORE  05 QUERY data           ____________________________
+
+            //using (var context = new AppDbContext())
+            //{
+            //    var items= context.Wallets.Where(x => x.Balance > 5000);
+            //    //Console.WriteLine(item);
+            //    foreach (var item in items)
+            //    {
+            //        Console.WriteLine(item);
+
+            //    }
+            //}
+            //__________________________  EF CORE  05 transaction            ____________________________
+
+            //var amoutToTransfer = 1000;
+
+            //using (var context = new AppDbContext())
+            //{
+            //    using (var transactionScopee = context.Database.BeginTransaction())
+            //    {
+            //        var from = context.Wallets.Single(x => x.Id == 2);
+            //        var to = context.Wallets.Single(x => x.Id == 3);
+
+            //        from.Balance -= amoutToTransfer;
+            //        context.SaveChanges();
+            //        to.Balance += amoutToTransfer;
+            //        context.SaveChanges();
+            //        transactionScopee.Commit();
+
+            //    }
+            //}
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
+}
