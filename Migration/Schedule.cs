857a409609d54cc820815f789920e97fb605019a
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-07-25 21:07:45 +0300

    Add files via upload

---
+++ Migration/Schedule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MigrationEFCore10;
+
+public partial class Schedule
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public bool Sun { get; set; }
+
+    public bool Mon { get; set; }
+
+    public bool Tue { get; set; }
+
+    public bool Wed { get; set; }
+
+    public bool Thu { get; set; }
+
+    public bool Fri { get; set; }
+
+    public bool Sat { get; set; }
+
+    public virtual ICollection<SectionSchedule> SectionSchedules { get; set; } = new List<SectionSchedule>();
+}
