a2bedd189bdbb2a0e0eacb28f541c4f36965a25f
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-07-25 21:08:52 +0300

    Add files via upload

---
+++ Migration/Office.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MigrationEFCore10;
+
+public partial class Office
+{
+    public int Id { get; set; }
+
+    public string OfficeName { get; set; } = null!;
+
+    public string OfficeLocation { get; set; } = null!;
+
+    public virtual ICollection<Instructor> Instructors { get; set; } = new List<Instructor>();
+}
